ca2914a83fa7710d954bc42c0c5a5108a9e35d54
Jose Angel <ah@xdevelop.mx>
2024-09-10 21:31:45 -0600

    initialData y placeholderData

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,16 +1,15 @@
-import { useQuery } from "@tanstack/react-query";
-import { getLabels } from "../actions";
+import { LoadingSpinner } from "../../shared";
+import { useLabels } from "../hooks";
 
 export const LabelPicker = () => {
 
-  const labelsQuery = useQuery({
-    queryKey: ['labels'],
-    queryFn: getLabels,
-  });
+  const { labelsQuery } = useLabels();
 
   if (labelsQuery.isLoading) {
     return (
-      <div className="flex justify-center items-center h-52">Loading...</div>
+      <div className="flex justify-center items-center h-52">
+        <LoadingSpinner />
+      </div>
     )
   }
 
@@ -20,7 +19,7 @@ export const LabelPicker = () => {
         labelsQuery.data?.map((label) => (
           <span
             key={label.id}
-            className="px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer"
+            className="animate-fadeIn px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer"
             style={{ 
               border: `1px solid #${label.color}`, 
               color: `#${label.color}`, 
---
+++ src/issues/hooks/useLabels.ts
@@ -0,0 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
+import { getLabels } from "../actions";
+import { GithubLabel } from "../interfaces";
+
+export const useLabels = () => {
+  const labelsQuery = useQuery({
+    queryKey: ["labels"],
+    queryFn: getLabels,
+    staleTime: 1000 * 60 * 60, // 1 hour,
+    placeholderData: [
+      {
+        id: 196858374,
+        node_id: "MDU6TGFiZWwxOTY4NTgzNzQ=",
+        url: "https://api.github.com/repos/facebook/react/labels/CLA%20Signed",
+        name: "CLA Signed",
+        color: "e7e7e7",
+        default: false,
+        description: null,
+      } satisfies GithubLabel,
+    ],
+    initialData: [
+      {
+        id: 196858374,
+        node_id: "MDU6TGFiZWwxOTY4NTgzNzQ=",
+        url: "https://api.github.com/repos/facebook/react/labels/CLA%20Signed",
+        name: "CLA Signed",
+        color: "e7e7e7",
+        default: false,
+        description: null,
+      } satisfies GithubLabel,
+    ],
+  });
+
+  return {
+    labelsQuery,
+  };
+};
