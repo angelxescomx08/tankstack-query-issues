da3b969bf4c391ca890c6566744086e2797417c5
Jose Angel <ah@xdevelop.mx>
2024-09-13 22:19:04 -0600

    since

--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { GithubIssue, State } from '../interfaces';
 import { useQueryClient } from '@tanstack/react-query';
 import { getIssue, getIssueComments } from '../actions';
+import { timeSince } from '../../helpers';
 
 type Props = {
   issue: GithubIssue;
@@ -54,7 +55,7 @@ export const IssueItem = ({ issue }:Props) => {
           {issue.title}
         </a>
         <span className="text-gray-500">
-          #{issue.number} opened 2 days ago by{' '}
+          #{issue.number} opened {timeSince(issue.created_at)} ago by{' '}
           <span className="font-bold">{issue.user.login}</span>
           <div className='flex flex-wrap'>
             {issue.labels.map((label) => (
